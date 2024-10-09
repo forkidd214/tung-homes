b90fa02a8a883b56f2613f6504e5d525af208cab
zoufei <forkidd214@gmail.com>
2024-10-10 01:28:15 +1030

    test: add smoke tests

--- e2e/smoke.spec.ts
+++ e2e/smoke.spec.ts
@@ -4,7 +4,7 @@ test('App loads and nav works', async ({ page }) => {
   await page.goto('/')
 
   // Click the logo link.
-  page.getByRole('link', { name: 'tung homes' }).click()
+  await page.getByRole('link', { name: 'tung homes' }).click()
 
   // Expects page to have a heading with the name of slogan.
   await expect(
