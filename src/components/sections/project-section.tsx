36409f214aa025f8e29abb185b964dbd057b0f10
zoufei <forkidd214@gmail.com>
2024-11-16 00:13:04 +1030

    refactor: extract ProjectCard component into its own file to reuse in project page

--- src/components/sections/project-section.tsx
+++ src/components/sections/project-section.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import React from 'react'
-import Image, { StaticImageData } from 'next/image'
-import Link from 'next/link'
 import { Slot } from '@radix-ui/react-slot'
 
 import { PROJECTS } from '@/data'
@@ -17,6 +15,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '../carousel'
+import ProjectCard from '../project-card'
 
 function ProjectSection() {
   const [api, setApi] = React.useState<CarouselApi>()
@@ -169,50 +168,6 @@ function GridItemWrapper({
       break
   }
   throw new Error(`<GridItemWrapper /> has invalid props ${{ item }}`)
-}
-
-function ProjectCard({
-  href,
-  title,
-  image,
-  index,
-  className,
-}: {
-  href: string
-  title: string
-  image: {
-    src: string | StaticImageData
-    alt: string
-    sizes: string
-  }
-  index?: number
-  className?: string
-}) {
-  return (
-    <Link href={href} aria-label={`Project ${index ?? ''}: ${title}`}>
-      <div className={cn('group relative overflow-hidden', className)}>
-        <div className="relative h-full transition duration-200 ease-linear group-hover:scale-110 group-hover:duration-1000">
-          <Image
-            src={image.src}
-            alt={image.alt}
-            fill
-            sizes={image.sizes}
-            style={{
-              objectFit: 'cover',
-            }}
-          />
-        </div>
-        <div className="absolute bottom-8 left-0 flex flex-row items-baseline justify-between gap-4 bg-primary/90 px-4 py-2 font-sans uppercase text-primary-foreground backdrop-blur">
-          {index !== undefined && (
-            <span className="font-semibold text-secondary-foreground brightness-200">
-              {index}
-            </span>
-          )}
-          <span>{title}</span>
-        </div>
-      </div>
-    </Link>
-  )
 }
 
 function ProgressRing({
